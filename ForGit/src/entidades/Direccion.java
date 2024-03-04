fd6cbe5a6175459ea072e623cd5d6772b5ce4a6f
AnthonyGu <ninfrad23@gmail.com>
2024-03-04 11:07:43 +0100

    add tostring

---
+++ ForGit/src/entidades/Direccion.java
@@ -0,0 +1,64 @@
+package entidades;
+
+public class Direccion {
+	
+	String tipoVia;
+	String nombreVia;
+	String localidad;
+	int CP;
+	
+	@Override
+	public String toString() {
+		return "Direccion [tipoVia=" + tipoVia + ", nombreVia=" + nombreVia + ", localidad=" + localidad + ", CP=" + CP
+				+ "]";
+	}
+
+	public Direccion() {
+		super();
+	}
+	
+	
+
+	public Direccion(String tipoVia, String nombreVia, String localidad, int cP) {
+		super();
+		this.tipoVia = tipoVia;
+		this.nombreVia = nombreVia;
+		this.localidad = localidad;
+		CP = cP;
+	}
+
+	public String getTipoVia() {
+		return tipoVia;
+	}
+
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+
+	public String getNombreVia() {
+		return nombreVia;
+	}
+
+	public void setNombreVia(String nombreVia) {
+		this.nombreVia = nombreVia;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public int getCP() {
+		return CP;
+	}
+
+	public void setCP(int cP) {
+		CP = cP;
+	}
+	
+	
+	
+}
