43eba5d961318af9a935aa3b23801a7050fb59ea
AnthonyGu <ninfrad23@gmail.com>
2024-03-04 11:08:21 +0100

    add contructores

--- ForGit/src/entidades/Direccion.java
+++ ForGit/src/entidades/Direccion.java
@@ -17,8 +17,6 @@ public class Direccion {
 		super();
 	}
 	
-	
-
 	public Direccion(String tipoVia, String nombreVia, String localidad, int cP) {
 		super();
 		this.tipoVia = tipoVia;
@@ -27,37 +25,9 @@ public class Direccion {
 		CP = cP;
 	}
 
-	public String getTipoVia() {
-		return tipoVia;
-	}
+	
 
-	public void setTipoVia(String tipoVia) {
-		this.tipoVia = tipoVia;
-	}
-
-	public String getNombreVia() {
-		return nombreVia;
-	}
-
-	public void setNombreVia(String nombreVia) {
-		this.nombreVia = nombreVia;
-	}
-
-	public String getLocalidad() {
-		return localidad;
-	}
-
-	public void setLocalidad(String localidad) {
-		this.localidad = localidad;
-	}
-
-	public int getCP() {
-		return CP;
-	}
-
-	public void setCP(int cP) {
-		CP = cP;
-	}
+	
 	
 	
 	
